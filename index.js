cc0eaa19d1474054362c7d7f691cb557101c2051
nil <nilanjandasgupta0@gmail.com>
2026-07-04 01:07:19 +0530

    msg

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
 const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first'); // Force IPv4 to fix Render's IPv6 issue with Gmail
+require('dotenv').config();
+
 
 const express = require('express');
 const cors = require('cors');
