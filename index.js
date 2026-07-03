1de16b2e27c4620977453a8b4e9ca9b714a5c13a
nil <nilanjandasgupta0@gmail.com>
2026-07-04 01:14:54 +0530

    msg

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ app.use(express.json());
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, 
+  port: 587,
+  secure: false, 
+  requireTLS: true
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
