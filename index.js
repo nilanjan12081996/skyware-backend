7ed1dc63230ec7e4891dc0254b024f8f1aa0d06c
nil <nilanjandasgupta0@gmail.com>
2026-07-04 01:11:25 +0530

    msg

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4,
   connectionTimeout: 10000, 
   greetingTimeout: 10000,
   socketTimeout: 10000,
